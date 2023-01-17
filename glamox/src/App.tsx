4574bc7dcb6f2af12c4a22ffc0cce539fa8d751f
rajeshdeo <raje754912@gmail.com>
2023-01-17 13:05:42 +0530

    added structure

---
+++ glamox/src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import './App.css';
+import { Display } from './Display';
+
+
+
+function App() {
+  
+  return (
+    <div className="App">
+        <Display />
+    </div>
+  );
+}
+
+export default App;
---
+++ glamox/src/Display.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const Display = () => {
+  return (
+    <div>Display</div>
+  )
+}
