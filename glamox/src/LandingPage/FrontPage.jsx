07f4030865992a7fac0d0b1031c59722c19d85fa
unknown <pradhand920@gmail.com>
2023-01-19 21:20:16 +0530

    added day-2

--- glamox/src/LandingPage/FrontPage.jsx
+++ glamox/src/LandingPage/FrontPage.jsx
@@ -60,7 +60,119 @@ const FrontPage = () => {
 
 
       {/* Biggest Payday Discount=================================ends */}
+       
+       {/* small headline start =================*/}
+       <div className='small_headline'>
+        <img src="https://images-static.nykaa.com/uploads/70338135-f026-4824-9c6d-0b8a267d6dbb.jpg?tr=w-2400,cm-pad_resize" alt="error" />
+       </div>
+       {/*small headline ends =====================  */}
 
+       {/* stockup category start*/}
+       
+       <div className='biggest_day_container'>
+        <div>
+            <img src="https://images-static.nykaa.com/uploads/3ecec5a4-7729-4abd-a36c-a34b4e45d067.jpg?tr=w-600,cm-pad_resize" alt="error" />
+
+        </div>
+        <div>
+        <img src="https://images-static.nykaa.com/uploads/3ecec5a4-7729-4abd-a36c-a34b4e45d067.jpg?tr=w-600,cm-pad_resize" alt="error" />
+
+        </div>
+        <div>
+        <img src="https://images-static.nykaa.com/uploads/3ecec5a4-7729-4abd-a36c-a34b4e45d067.jpg?tr=w-600,cm-pad_resize" alt="error" />
+
+        </div>
+        <div>
+             <img src="https://images-static.nykaa.com/uploads/3ecec5a4-7729-4abd-a36c-a34b4e45d067.jpg?tr=w-600,cm-pad_resize" alt="error" />
+        </div>
+       </div>
+       <br />
+       {/* stockup category ends  */}
+       {/* sale-ary saver start===================== */}
+       <h1>Sale-ary Savers</h1>
+    
+       
+       <div className="sale_ary_saver_container">
+        {/* first part */}
+       <div>
+            <img src="https://images-static.nykaa.com/uploads/38b00c46-1cb7-494e-947f-c3285ee4510d.jpeg?tr=w-480,cm-pad_resize" alt="error" />
+            <h3>Up To 25% Off</h3>
+            <h3 className="greycolor">On Entire Range</h3>
+        </div>
+        <div>
+        <img src="https://images-static.nykaa.com/uploads/86f26ba4-7533-4fae-a09a-94b1b5962e69.jpeg?tr=w-480,cm-pad_resize" alt="error" />
+            <h3>Free Hair Masks</h3>
+            <h3 className="greycolor">On ₹ 1499+ </h3>
+        </div>
+        <div>
+        <img src="https://images-static.nykaa.com/uploads/7502359d-d0de-4a1f-980d-8a6dbe0632b6.jpeg?tr=w-480,cm-pad_resize" alt="error" />
+            <h3>Up To 20% Off</h3>
+            <h3 className="greycolor">On Best Seller</h3>
+        </div>
+        <div>
+        <img src="https://images-static.nykaa.com/uploads/618c8c4c-28b4-44d5-968b-7135c502a902.jpeg?tr=w-480,cm-pad_resize" alt="error" />
+            <h3>Up To 25 % Off</h3>
+            <h3 className="greycolor">Free Scrum on ₹ 899</h3>
+        </div>
+        <div>
+        <img src="https://images-static.nykaa.com/uploads/31b55381-072b-4495-a8f7-a3ab9ffc5779.jpeg?tr=w-480,cm-pad_resize" alt="error" />
+            <h3>Up To 25% Off</h3>
+            <h3 className="greycolor">On Entire Range</h3>
+        </div>
+        {/* second part */}
+        <div>
+            <img src="https://images-static.nykaa.com/uploads/3fd4fd3c-9910-4150-8919-431a94ae38f1.jpeg?tr=w-480,cm-pad_resize" alt="error" />
+            <h3>Up To 25% Off</h3>
+            <h3 className="greycolor">On Entire Range</h3>
+        </div>
+        <div>
+        <img src="https://images-static.nykaa.com/uploads/0a3ff301-caa3-4649-ae28-5d97c5a4e743.jpeg?tr=w-480,cm-pad_resize" alt="error" />
+            <h3>Free Hair Masks</h3>
+            <h3 className="greycolor">On ₹ 1499+ </h3>
+        </div>
+        <div>
+        <img src="https://images-static.nykaa.com/uploads/bc287e7e-14de-47a6-b1a5-2435e9457cfd.jpeg?tr=w-480,cm-pad_resize" alt="error" />
+            <h3>Up To 20% Off</h3>
+            <h3 className="greycolor">On Best Seller</h3>
+        </div>
+        <div>
+        <img src="https://images-static.nykaa.com/uploads/4418ed4b-f631-4cd1-bf53-2b2c3c066c63.jpeg?tr=w-480,cm-pad_resize" alt="error" />
+            <h3>Up To 25 % Off</h3>
+            <h3 className="greycolor">Free Scrum on ₹ 899</h3>
+        </div>
+        <div>
+        <img src="https://images-static.nykaa.com/uploads/3c2127e7-3123-450d-b94e-f69a44f64a9f.jpeg?tr=w-480,cm-pad_resize" alt="error" />
+            <h3>Up To 25% Off</h3>
+            <h3 className="greycolor">On Entire Range</h3>
+        </div>
+        {/* third part */}
+        <div>
+            <img src="https://images-static.nykaa.com/uploads/1aa0dd08-d0f3-49c9-a575-79585743114f.jpeg?tr=w-480,cm-pad_resize" alt="error" />
+            <h3>Up To 25% Off</h3>
+            <h3 className="greycolor">On Entire Range</h3>
+        </div>
+        <div>
+        <img src="https://images-static.nykaa.com/uploads/3fd4fd3c-9910-4150-8919-431a94ae38f1.jpeg?tr=w-480,cm-pad_resize" alt="error" />
+            <h3>Free Hair Masks</h3>
+            <h3 className="greycolor">On ₹ 1499+ </h3>
+        </div>
+        <div>
+        <img src="https://images-static.nykaa.com/uploads/893c0817-51ea-4c7a-9a77-b16d24abebfd.jpeg?tr=w-480,cm-pad_resize" alt="error" />
+            <h3>Up To 20% Off</h3>
+            <h3 className="greycolor">On Best Seller</h3>
+        </div>
+        <div>
+        <img src="https://images-static.nykaa.com/uploads/64332291-6e7f-40ad-a194-9eb5d69f6c6c.jpeg?tr=w-480,cm-pad_resize" alt="error" />
+            <h3>Up To 25 % Off</h3>
+            <h3 className="greycolor">Free Scrum on ₹ 899</h3>
+        </div>
+        <div>
+        <img src="https://images-static.nykaa.com/uploads/55d82855-1c99-406c-aef1-c313a79f7a35.jpeg?tr=w-480,cm-pad_resize" alt="error" />
+            <h3>Up To 25% Off</h3>
+            <h3 className="greycolor">On Entire Range</h3>
+        </div>
+       </div>
+       {/* sale-ary saver ends ===================== */}
 
     </div>
   )
