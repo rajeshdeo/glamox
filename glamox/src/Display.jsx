d2ecad27853d97bba760b644668f8e679fa2c7e8
rajeshdeo <raje754912@gmail.com>
2023-01-19 12:17:54 +0530

    added template

--- glamox/src/Display.jsx
+++ glamox/src/Display.jsx
@@ -1,7 +1,14 @@
 import React from 'react'
+import { AllRoutes } from './Pages/AllRoutes'
+import { Footer } from './Pages/Footer'
+import { Navbar } from './Pages/Navbar'
 
 export const Display = () => {
   return (
-    <div>Display</div>
+    <div>
+      <Navbar />
+      <AllRoutes />
+      <Footer />
+    </div>
   )
 }
