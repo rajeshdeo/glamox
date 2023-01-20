cf5b4a9a63a14d8f784b3b6bd030fcb3e144b542
Jagroshan <jagroshansingh615@gmail.com>
2023-01-21 01:25:54 +0530

    pulled the deepak pradhan's code

--- glamox/src/index.tsx
+++ glamox/src/index.tsx
@@ -4,8 +4,9 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
-import { Provider } from 'react-redux';
 import { store } from './ReduxStore/Store';
+import { Provider } from 'react-redux';
+
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
