13aca151877de896a28d6a508a2129a08c261ba7
Faizan8092 <ali.faizan2015@gmail.com>
2023-01-22 00:03:39 +0530

    sun

--- glamox/src/Pages/AllRoutes.jsx
+++ glamox/src/Pages/AllRoutes.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import {Routes,Route} from "react-router-dom";
 import FrontPage from '../LandingPage/FrontPage';
-import { Home } from './Home';
+
 import Login from './Login';
 import Signup from './Signup';
 export const AllRoutes = () => {
