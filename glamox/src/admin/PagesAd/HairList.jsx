10b58692b92869d40eab2ae7edda13834e011734
TejasBishnoi21 <tejas.vishnoi@gmail.com>
2023-01-21 14:45:54 +0530

    day-5_commit

---
+++ glamox/src/admin/PagesAd/HairList.jsx
@@ -0,0 +1,9 @@
+
+
+const HairAd = ()=>{
+    return <div>
+        <h2>Hair Admin Page</h2>
+    </div>
+}
+
+export default HairAd;
---
+++ glamox/src/admin/PagesAd/SkinList.jsx
@@ -0,0 +1,9 @@
+
+
+const SkinAd = ()=>{
+    return <div>
+        <h2>Skin Admin Page</h2>
+    </div>
+}
+
+export default SkinAd;
