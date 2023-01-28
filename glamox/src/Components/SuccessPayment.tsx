4a68eeead2e388053e153c3d8c150bc82ed0a003
Jagroshan <jagroshansingh615@gmail.com>
2023-01-28 12:49:28 +0530

    added skeleton to product page and cart empty after successful purchase

--- glamox/src/Components/SuccessPayment.tsx
+++ glamox/src/Components/SuccessPayment.tsx
@@ -1,9 +1,26 @@
-import { Box, Button, Heading, Text, VStack } from '@chakra-ui/react';
+import { Box, Button, Heading, Text, useToast, VStack } from '@chakra-ui/react';
 import { CheckCircleIcon } from '@chakra-ui/icons';
 import { useNavigate } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import { useEffect } from 'react';
+import { emptyCart } from '../ReduxStore/CartStore/acton';
+import Alert from './Alert';
 
 export default function SuccessPayment() {
+  const dispatch=useDispatch();
   const navigate=useNavigate()
+  const toast = useToast()
+  let alertdata = {
+    title: 'Hurrah...',
+    description: "Order successfully placed",
+    status: 'success',
+}
+
+ useEffect(()=>{
+  toast(Alert(alertdata))
+  dispatch(emptyCart())
+ },[])
+
   return (
     <Box textAlign="center" py={10} px={6} margin='100px 0px'>
       <CheckCircleIcon boxSize={'50px'} color={'green.500'} />
@@ -15,7 +32,7 @@ export default function SuccessPayment() {
         Purchase Details has been send to your email address.
       </Text>
       <Button colorScheme='teal' variant='outline' onClick={()=>navigate('/')}>
-        For More Shopping...
+        Continue Shopping
       </Button>
       </VStack>
     </Box>
